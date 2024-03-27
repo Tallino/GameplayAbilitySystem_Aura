95d23e619153ec81e3a17e01138b400c37d28d0a
Giancarlo Danese <gdanese@hotmail.it>
2024-03-27 22:43:49 +0100

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Tritaghiaccio Games S.r.l
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Tritaghiaccio Games S.r.l
+
+#pragma once
+
+#include "CoreMinimal.h"
+
