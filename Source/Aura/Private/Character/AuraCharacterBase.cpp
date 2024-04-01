9f6ef895ff0d2bba44dc5156656d3cbf9c0bad67
Tallido <giancarlo.danese@mail.polimi.it>
2024-04-01 23:54:13 +0200

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright Tritaghiaccio Games S.r.l
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
