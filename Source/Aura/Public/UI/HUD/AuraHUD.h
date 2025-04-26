6c3b85dd9db420f53d9b23bab320ed7693af0923
Giancarlo Danese <gdanese@hotmail.it>
2025-04-26 16:11:01 +0200

    Aura HUD

---
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,30 @@
+// Copyright Tritaghiaccio Games S.r.l
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetCLass;
+
+protected:
+	virtual void BeginPlay() override;
+};
