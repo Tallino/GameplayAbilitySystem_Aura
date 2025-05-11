60597130e09bae365d6f10f8b98384dd182a56a2
Giancarlo Danese <gdanese@hotmail.it>
2025-05-11 13:03:12 +0200

    PostGameplayEffectExecute

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,7 +2,11 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
@@ -34,6 +38,46 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
 	{
 		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
 	}
+}
+void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectPropertiesEnhanced& Props) const
+{
+	// Source = causer of the effect, Target = target of the effect (owner of this AS)
+	
+	Props.SourceProperties->EffectContextHandle = Data.EffectSpec.GetContext();
+	Props.SourceProperties->AbilitySystemComponent = Props.SourceProperties->EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if (IsValid(Props.SourceProperties->AbilitySystemComponent) && Props.SourceProperties->AbilitySystemComponent->AbilityActorInfo.IsValid() && IsValid(Props.SourceProperties->AbilitySystemComponent->GetAvatarActor()))
+	{
+		Props.SourceProperties->AvatarActor = Props.SourceProperties->AbilitySystemComponent->GetAvatarActor();
+		Props.SourceProperties->Controller = Props.SourceProperties->AbilitySystemComponent->AbilityActorInfo->PlayerController.Get();
+		if (Props.SourceProperties->Controller == nullptr && Props.SourceProperties->AvatarActor != nullptr)
+		{
+			if (const APawn* Pawn = Cast<APawn>(Props.SourceProperties->AvatarActor))
+			{
+				Props.SourceProperties->Controller = Pawn->GetController();
+			}
+		}
+		if (Props.SourceProperties->Controller)
+		{
+			Props.SourceProperties->Character = Cast<ACharacter>(Props.SourceProperties->Controller->GetPawn());
+		}
+	}
+
+	if (Data.Target.AbilityActorInfo.IsValid() && IsValid(Data.Target.GetAvatarActor()))
+	{
+		Props.TargetProperties->AvatarActor = Data.Target.GetAvatarActor();
+		Props.TargetProperties->Controller = Data.Target.AbilityActorInfo->PlayerController.Get();
+		Props.TargetProperties->Character = Cast<ACharacter>(Props.TargetProperties->AvatarActor);
+		Props.TargetProperties->AbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetProperties->AvatarActor);
+	}
+}
+
+void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+	
+	FEffectPropertiesEnhanced Props;
+	SetEffectProperties(Data, Props);
 }
 
 // Rep notifier enable us to still read the old variable before replication
