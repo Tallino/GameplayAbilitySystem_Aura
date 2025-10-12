6541cc105876c41009467feae68b495925b4b9b9
Giancarlo Danese <gdanese@hotmail.it>
2025-10-12 10:43:52 +0200

    Player Level and Combat Interface

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -19,9 +19,12 @@ class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInte
 
 public:
 	AAuraPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet;}
 
+	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	
 protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
@@ -29,4 +32,11 @@ protected:
 	//AttributeSet on PlayerState is for controlled pawns
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
+	
+private:
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
+	int32 Level = 1;
+
+	UFUNCTION()
+	void OnRep_Level(int32 OldLevel);
 };
