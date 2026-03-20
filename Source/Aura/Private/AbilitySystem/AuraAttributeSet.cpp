d0711399182a98db89b75a64f83b9bd8c14ef065
Giancarlo Danese <gdanese@hotmail.it>
2026-03-20 16:04:11 +0100

    Activating the Enemy Hit React Ability

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -149,6 +149,12 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			SetHealth(FMath::Clamp(NewHealth, 0.f, GetMaxHealth()));
 
 			const bool bFatal = NewHealth <= 0.f;
+			if (!bFatal)
+			{
+				FGameplayTagContainer TagContainer;
+				TagContainer.AddTag(FAuraGameplayTags::Get().Effects_HitReact);
+				Props.TargetProperties->AbilitySystemComponent->TryActivateAbilitiesByTag(TagContainer); 
+			}
 		}
 	}
 }
