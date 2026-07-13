fb1aed50c470ad2c9665f7ddef3e35eabe3f8099
Giancarlo Danese <gdanese@hotmail.it>
2026-07-13 11:36:57 +0200

    Ability Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/AbilityInfo.cpp
@@ -0,0 +1,24 @@
+// Copyright Tritaghiaccio Games S.r.l
+
+
+#include "AbilitySystem/Data/AbilityInfo.h"
+
+#include "Aura/AuraLogChannels.h"
+
+FAuraAbilityInfo UAbilityInfo::FindAbilityInfoForTag(const FGameplayTag& AbilityTag, bool bLogNotFound)
+{
+	for (const auto& Info : AbilityInformation)
+	{
+		if (Info.AbilityTag == AbilityTag)
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogAura, Error, TEXT("Can't find info for AbilityTag [%s] on AbilityInfo [%s]"), *AbilityTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAbilityInfo();
+}
--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -24,6 +24,9 @@ struct FUIWidgetRow : public FTableRowBase
 	UTexture2D* Image = nullptr;
 };
 
+class UAuraUserWidget;
+class UAbilityInfo;
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
@@ -58,6 +61,9 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UAbilityInfo> AbilityInfo;
+	
 	template <typename T>
 	T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 };
